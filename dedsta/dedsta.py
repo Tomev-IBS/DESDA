db3be076ca9e616d947a68fb0a0fff4095b2ef17
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2023-10-22 22:14:24 +0200

    feat(algorithm): Create project structure.

---
+++ dedsta/dedsta.py
@@ -0,0 +1,15 @@
+__author__ = "Tomasz Rybotycki"
+
+
+from KDEpy import BaseKDE
+
+
+class DEDSTA:
+    """
+    DEDSTA stands for (D)ensity (E)stitmation for (D)ata (S)treams with (T)tends
+    (A)lgorithm. As the name suggests, it does exactly that.
+
+    The implementation is based on KDEpy library.
+    """
+    def __init__(self) -> None:
+        pass
