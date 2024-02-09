604ed9e87b50baa5624157b4c138e37a3fc231e0
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2024-02-09 19:58:41 +0100

    feat(modules): Add initial version of the DEDSTA aging module and the modules handling in DEDSTA algorithm.

--- dedsta/structure.py
+++ dedsta/structure.py
@@ -1,10 +1,8 @@
 __author__ = "Tomasz Rybotycki"
 
-import numpy as np
-from KDEpy.kernel_funcs import Kernel, gaussian
 from typing import List
 import abc
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike
 
 
 class DEDSTADataPoint:
@@ -12,8 +10,14 @@ class DEDSTADataPoint:
     Class representing a single data point in DEDSTA algorithm. The value can either
     be a single number or a vector.
     """
-    def __init__(self, value: NDArray[float]):
-        self.value: NDArray[float] = value
+    def __init__(self, value: ArrayLike):
+        """
+        Initializes the data point.
+
+        :param value:
+            New data point sampled from the stream.
+        """
+        self.value: ArrayLike = value
         self.weight: float = 1.0
 
 
@@ -22,25 +26,40 @@ class DEDSTAReservoir(abc.ABC):
     Class representing a reservoir used in DEDSTA algorithm.
     """
     def __init__(self, min_size: int, max_size: int) -> None:
+        """
+        Initializes the reservoir.
+
+        :param min_size:
+            Minimum size of the reservoir.
+        :param max_size:
+            Maximum size of the reservoir.
+        """
         self.min_size: int = min_size
         self.max_size: int = max_size
 
         self.data_points: List[DEDSTADataPoint] = list()
 
-    @abc.abstractmethod
-    def add(self, data_point: NDArray[float]) -> None:
+    def add(self, data_point: ArrayLike) -> None:
         """
         Adds a new data point to the reservoir.
 
         :param data_point:
-            New data point.
+            New data point sampled from the stream.
+        """
+        if self.size() >= self.max_size:
+            self.remove()
 
-        :return:
-            None.
+        self.data_points.insert(0, DEDSTADataPoint(data_point))
+
+    @abc.abstractmethod
+    def remove(self) -> None:
+        """
+        Removes a data point from the reservoir. The choice of the data point depends
+        on the implementation.
         """
         raise NotImplementedError
 
-    def get_points(self) -> List[NDArray[float]]:
+    def get_points(self) -> List[ArrayLike]:
         """
         Returns all points from the reservoir.
 
@@ -58,7 +77,20 @@ class DEDSTAReservoir(abc.ABC):
         """
         return [point.weight for point in self.data_points]
 
+    def reset_weights(self) -> None:
+        """
+        Resets all weights in the reservoir.
+        """
+        for point in self.data_points:
+            point.weight = 1.0
+
     def size(self) -> int:
+        """
+        Returns the size of the reservoir.
+
+        :return:
+            Size of the reservoir.
+        """
         return len(self.data_points)
 
 
@@ -67,20 +99,65 @@ class DEDSTASlidingWindowReservoir(DEDSTAReservoir):
     Class representing a sliding window variant of the reservoir used in DEDSTA
     algorithm.
     """
-    def __init__(self, min_size: int, max_size:int) -> None:
+    def __init__(self, min_size: int, max_size: int) -> None:
+        """
+        Initializes the reservoir.
+
+        :param min_size:
+            Minimum size of the reservoir.
+        :param max_size:
+            Maximum size of the reservoir.
+        """
         super().__init__(min_size, max_size)
 
-    def add(self, data_point: NDArray[float]) -> None:
+    def remove(self) -> None:
         """
-        Adds a new data point to the reservoir.
-
-        :param data_point:
-            New data point.
-
-        :return:
-            None.
+        Removes the last data point from the reservoir.
         """
-        if self.size() >= self.max_size:
-            self.data_points.pop(0)
+        self.data_points.pop(-1)
 
-        self.data_points.append(DEDSTADataPoint(data_point))
+
+class DEDSTAModule(abc.ABC):
+    """
+    Abstract class representing a module in DEDSTA algorithm.
+    """
+    def __init__(self, reservoir: DEDSTAReservoir) -> None:
+        self.reservoir: DEDSTAReservoir = reservoir
+
+    @abc.abstractmethod
+    def apply(self, nonstationarity: float) -> None:
+        """
+        Applies the module to modify the weights of the data points in the reservoir.
+
+        :param nonstationarity:
+            The degree of nonstationarity. It should be a number between 0 and 1.
+        """
+        raise NotImplementedError
+
+
+class DEDSTAAgingModule(DEDSTAModule):
+    """
+    Class representing a module responsible for updating weights in DEDSTA algorithm.
+    """
+    def __init__(self, reservoir: DEDSTAReservoir) -> None:
+        super().__init__(reservoir)
+
+    def apply(self, nonstationarity: float) -> None:
+        """
+        Updates weights of all data points in the reservoir.
+
+        :note:
+            It assumes that the new element is inserted at the beginning of the
+            reservoir.
+
+        :param nonstationarity:
+            Nonstationarity degree.
+        """
+        n_elements: int = self.reservoir.size()
+
+        for i in range(n_elements):
+            self.reservoir.data_points[i].weight = (
+                2 * (1 - i * nonstationarity / n_elements)
+            )
+
+
